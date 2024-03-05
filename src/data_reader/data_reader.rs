ff0cc6ea603a0de312b31a37dff49e4f70fa706b
= <michaekluska@gmail.com>
2024-03-05 11:27:33 +0100

    Complete implementation of data_reader

--- src/data_reader/data_reader.rs
+++ src/data_reader/data_reader.rs
@@ -14,6 +14,12 @@ pub enum Endianness {
     Big,
 }
 
+pub enum EWhence {
+    Start,
+    End,
+    Current,
+}
+
 impl<'a> DataReader<'a> {
     pub fn new(data: &'a [u8], endianness: Endianness) -> Self {
         Self {
@@ -31,5 +37,55 @@ impl<'a> DataReader<'a> {
 
     pub fn read_i8(&mut self) -> u8 {
         self.get_u8() as i8
+    }
+
+    pub fn read_u16(&mut self) -> u16 {
+        match (self.endianness) {
+            Endianness::Little => self.cursor.read_u16::<LittleEndian>().unwrap(),
+            Endianness::Big => self.cursor.read_u16::<BigEndian>().unwrap(),
+        }
+    }
+
+    pub fn read_i16(&mut self) -> i16 {
+        self.get_u16() as i16
+    }
+
+    pub fn read_u32(&mut self) -> u32 {
+        match (self.endianness) {
+            Endianness::Little => self.cursor.read_u32::<LittleEndian>().unwrap(),
+            Endianness::Big => self.cursor.read_u32::<BigEndian>().unwrap(),
+        }
+    }
+
+    pub fn read_i32(&mut self) -> i32 {
+        self.get_u32() as i32
+    }
+
+    pub fn read_f32(&mut self) -> f32 {
+        match (self.endianness) {
+            Endianness::Little => self.cursor.read_f32::<LittleEndian>().unwrap(),
+            Endianness::Big => self.cursor.read_f32::<BigEndian>().unwrap(),
+        }
+    }
+
+    pub fn read_f64(&mut self) -> f64 {
+        match (self.endianness) {
+            Endianness::Little => self.cursor.read_f64::<LittleEndian>().unwrap(),
+            Endianness::Big => self.cursor.read_f64::<BigEndian>().unwrap(),
+        }
+    }
+
+    pub fn read_bytes(&mut self, size: usize) -> Vec<u8> {
+        let mut buffer = vec![0; size];
+        self.cursor.read_exact(&mut buffer).unwrap();
+        buffer
+    }
+
+    pub fn seek(&mut self, whence: EWhence, pos: usize) {
+        match whence {
+            EWhence::Start => self.cursor.set_position(pos as u64),
+            EWhence::End => self.cursor.set_position((self.data.len() - pos) as u64),
+            EWhence::Current => self.cursor.set_position(self.cursor.position() + pos as u64),
+        }
     }
 }
